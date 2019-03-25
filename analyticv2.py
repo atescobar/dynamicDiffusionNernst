3c3f9d296d5ada6406c788e54664247262f243eb
Agustin Escobar <agustin.escoblanc@gmail.com>
2019-03-24 22:43:16 -0300

    Initial Commit

---
+++ analyticv2.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def C_an(xi, tau):
+    # note that tau = D t / xb ** 2, where t is the time
+    Cb = 100
+    total = 0
+    tolMax  = 1e-10 
+    tolMin = tolMax*1e-2
+    m = 0
+    cond = True
+    previo = 10 * np.ones(len(xi))
+    
+    while(cond):
+        Am = ((-1) ** m ) / ( 2 * m + 1 ) * np.exp( - ( ( 2 * m + 1 ) * ( np.pi / 2 ) ) ** 2 * tau ) 
+        actual = Am * np.cos( (2 * m + 1) * np.pi / 2 * xi )
+        total += actual
+        m = m + 1
+        
+        if (m > 1):
+            contrib = (actual)/previo
+            for i in range(0, len(xi)-1):
+#                if ( np.abs(contrib[i]) > tolMin):
+#                    cond = True
+                if ( np.abs(max(contrib)) < tolMax):
+                    
+                    cond = False
+                        
+        previo = actual
+    print("Series truncation at n = " + str(m) + " with maximnum local error " + str(np.amax(np.abs(contrib))) + "%")
+    return  Cb - ( (4 * Cb / np.pi ) * total ) 
+
+
+
+
+
+
+
